70136a6945179e2cd1881cd73510c42719aeae07
hideo651 <hideo651@msn.com>
2023-03-08 16:55:20 -0300

    feat: criação do carrinho de compras funcionando

--- carts/serializer.py
+++ carts/serializer.py
@@ -1,6 +1,22 @@
 from rest_framework import serializers
+from .models import Cart, Cart_Product
 
 
 class CartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cart
+        fields = ["id", "user_id", "cart_total", "products"]
+        depth = 1
+
     def __str__(self):
+        return ""
+
+
+class CartProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cart_Product
+        fields = ["id", "product", "cart_id", "quantity"]
+        depth = 1
+
+    def __str__(self) -> str:
         return ""
--- carts/views.py
+++ carts/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
-from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
-from .models import Cart
-from .serializer import CartSerializer
+from rest_framework.generics import (
+    CreateAPIView,
+    RetrieveUpdateDestroyAPIView,
+    ListCreateAPIView,
+    DestroyAPIView,
+)
+from .models import Cart, Cart_Product
+from .serializer import CartSerializer, CartProductSerializer
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
 class CartView(CreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
     queryset = Cart.objects.all()
     serializer_class = CartSerializer
+
+    def perform_create(self, serializer):
+        return serializer.save(user_id=self.request.user.id)
 
 
 class CartDetailView(RetrieveUpdateDestroyAPIView):
@@ -14,3 +27,14 @@ class CartDetailView(RetrieveUpdateDestroyAPIView):
     permission_classes = []
     queryset = Cart.objects.all()
     serializer_class = CartSerializer
+
+
+class AddProduct(ListCreateAPIView, DestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    queryset = Cart_Product.objects.all()
+    serializer_class = CartProductSerializer
+
+    def perform_create(self, serializer):
+        return serializer.save(cart_id=self.kwargs["pk"])
