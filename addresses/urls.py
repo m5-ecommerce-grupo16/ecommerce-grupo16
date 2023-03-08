4403a8d026e569c1a35af855a71098ac7606e37f
juliomello93 <juliomello@live.com>
2023-03-08 12:46:48 -0300

    fix urls, from _id to pk

--- addresses/urls.py
+++ addresses/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from . import views
 
+from . import views
 
 urlpatterns = [
     path("address/", views.AddressView.as_view()),
-    path("address/<int:address_id>/", views.AddressDetailView.as_view()),
+    path("address/<int:pk>/", views.AddressDetailView.as_view()),
 ]
