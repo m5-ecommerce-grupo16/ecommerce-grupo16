daad99922a176b0526ff44678891c33aa7407c81
Syxh0wN <leonardobennr@gmail.com>
2023-03-09 12:38:24 -0300

    feat: correct users_carts

--- carts/views.py
+++ carts/views.py
@@ -1,40 +1,45 @@
 from django.shortcuts import render
 from rest_framework.generics import (
     CreateAPIView,
-    RetrieveUpdateDestroyAPIView,
     ListCreateAPIView,
-    DestroyAPIView,
 )
 from .models import Cart, Cart_Product
 from .serializer import CartSerializer, CartProductSerializer
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 
 
-class CartView(CreateAPIView):
+class CartView(ListCreateAPIView):
     authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticated, IsAuthenticatedOrReadOnly]
 
-    queryset = Cart.objects.all()
     serializer_class = CartSerializer
 
-    def perform_create(self, serializer):
-        return serializer.save(user_id=self.request.user.id)
+    def get_queryset(self):
+        print(self.request.user.cart)
 
 
-class CartDetailView(RetrieveUpdateDestroyAPIView):
-    authentication_classes = []
-    permission_classes = []
-    queryset = Cart.objects.all()
-    serializer_class = CartSerializer
-
-
-class AddProduct(ListCreateAPIView, DestroyAPIView):
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticatedOrReadOnly]
-
+class CartAddView(CreateAPIView):
     queryset = Cart_Product.objects.all()
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated, IsAuthenticatedOrReadOnly]
+
     serializer_class = CartProductSerializer
+    serializer_class = CartSerializer
 
     def perform_create(self, serializer):
-        return serializer.save(cart_id=self.kwargs["pk"])
+        cart_queryset = Cart.objects.filter(user_id=self.request.user.id)
+
+        if cart_queryset.count() == 0:
+            Cart.objects.create(user_id=self.request.user.id)
+            cart_queryset = Cart.objects.filter(user_id=self.request.user.id)
+
+        cart_id = cart_queryset[0].id
+
+        cart_product_exist = Cart_Product.objects.filter(
+            cart_id=cart_id, product_id=self.request.data["product"])
+
+        if cart_product_exist.count() == 0:
+            serializer.save(cart_id=cart_id)
+        else:
+            print("product exist")
