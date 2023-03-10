f730f8eacfcc6c57a4ed3fc34f606212cbbe1bd9
juliomello93 <juliomello@live.com>
2023-03-10 14:41:37 -0300

    fix all orders

--- orders/views.py
+++ orders/views.py
@@ -11,7 +11,7 @@ from .models import Order, Order_Product
 from carts.models import Cart, Cart_Product
 from .serializer import OrderSerializer, Order_ProductSerializer
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from django.shortcuts import get_object_or_404
+from orders.models import Status
 
 
 class OrderView(ListCreateAPIView):
@@ -33,37 +33,5 @@ class AddOrder(ListCreateAPIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = Order_ProductSerializer
 
-    def create(self, request, *args, **kwargs):
-        # Criando a ordem
-        order = Order.objects.create(user=self.request.user)
-
-        # Adicionando produtos na ordem
-        cart = Cart.objects.get(user=self.request.user)
-        cart_products = Cart_Product.objects.filter(cart=cart)
-
-        for cart_product in cart_products:
-            product = cart_product.product
-            print(cart_product)
-            quantity = cart_product.ammount
-            if product.quantity >= quantity:
-                Order_Product.objects.create(order=order, product=product)
-                product.quantity -= quantity
-                product.save()
-            else:
-                # Caso não tenha quantidade suficiente, retornamos uma mensagem de erro
-                return Response(
-                    {
-                        "error": "Não há quantidade suficiente do produto "
-                        + product.name
-                    },
-                    status=status.HTTP_400_BAD_REQUEST,
-                )
-
-        # Atualizando o total da ordem
-        order.save()
-
-        # Deletando o carrinho do usuário
-        cart_products.delete()
-
-        serializer = self.serializer_class(order)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user, status=Status.SUCCESS)
