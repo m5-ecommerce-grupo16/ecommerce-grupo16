eaa8d4e8ec452eefb971acc4c4d97997a90a4c67
hideo651 <hideo651@msn.com>
2023-03-13 13:12:35 -0300

    feat: email sendo enviado automaticamente

--- orders/serializer.py
+++ orders/serializer.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
 from .models import Order, Order_Product
 from products.serializer import SummaryProductsSerilizer
-from carts.models import Cart_Product
-from rest_framework.exceptions import APIException
+from django.core.mail import EmailMessage
+from django.conf import settings
+from django.template.loader import render_to_string
 
 
 class OrderSerializer(serializers.ModelSerializer):
@@ -11,11 +12,27 @@ class OrderSerializer(serializers.ModelSerializer):
         fields = ["user_id", "id", "status", "date", "products"]
         depth = 1
 
+    def update(self, instance, validated_data):
+        status = validated_data.pop("status")
+        name = self.context["request"].user.first_name
+        template = f"Obrigado pela compra {name}, o status do seu pedido foi alterado para: {status}"
+        email = EmailMessage(
+            "Valeu pela compra",
+            template,
+            settings.EMAIL_HOST_USER,
+            [self.context["request"].user.email],
+        )
+
+        email.fail_silenylu = False
+        email.send()
+        return super().update(instance, validated_data)
+
 
 class ProductSeriallizer_Order(serializers.ModelSerializer):
     class Meta:
         model = Order_Product
         fields = ["ammount", "product"]
+        read_only_fields = ["ammount", "product"]
         depth = 1
 
     product = SummaryProductsSerilizer(read_only=True)
@@ -32,24 +49,8 @@ class Order_ProductSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = validated_data.pop("user")
+        products = user.cart.products
         order = Order.objects.create(**validated_data, user=user)
-        cart_products = Cart_Product.objects.filter(cart_id=user.cart.id)
 
-        for cart_product in cart_products:
-            Order_Product.objects.create(
-                product=cart_product.product, order=order, ammount=cart_product.ammount
-            )
-            cart_product.product.quantity -= cart_product.ammount
-
-            if cart_product.product.quantity <= cart_product.ammount:
-                error = APIException(
-                    "Não tem estoque suficiente para o produto"
-                    + cart_product.product.name
-                )
-                error.status_code = 400
-                raise error
-
-            cart_product.product.save()
-            cart_product.delete()
-
+        order.products.set(products.all())
         return order
